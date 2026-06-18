a48c099f829b9cc46e560ca394378fa8c9bef540
Michael E. Ershov <i@emershov.ru>
2026-06-18 20:19:11 +0800

    added eslint and prettier settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,31 +5,41 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    {
+        ignores: ['eslint.config.mjs', 'dist', 'node_modules'],
     },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+
+    eslintPluginPrettierRecommended,
+
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+            sourceType: 'commonjs',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
-  },
+
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/no-unsafe-argument': 'error',
+            '@typescript-eslint/no-unsafe-assignment': 'error',
+            '@typescript-eslint/no-unsafe-member-access': 'error',
+            '@typescript-eslint/no-unsafe-return': 'error',
+            '@typescript-eslint/no-floating-promises': 'warn',
+            '@typescript-eslint/await-thenable': 'error',
+            '@typescript-eslint/no-unused-vars': 'error',
+            'no-console': 'warn',
+            'prettier/prettier': ['error', { endOfLine: 'auto' }],
+        },
+    },
 );
